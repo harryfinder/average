a3e3a32ddd1e5811752b7ad66a5f4896e0443d2c
Harry Saidov <harryimac@Harrys-MacBook-Air.local>
2023-04-06 18:30:21 +0500

    инициализирован модуль

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -12,3 +12,35 @@ type Payment struct {
 	Amount   Money
 	Category Category
 }
+
+// PaymentSource представяет из себя источник оплаты
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
+}
+
+// Card представдяет информацию о платежной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+// Currency представляет собой денежную единицу
+type Currency string
+
+// коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN представляет номер карты
+type PAN string
